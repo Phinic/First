e9f46fcf5a50cffc0e438c32d42d0248a9946b32
Phinic <machinery69@gmail.com>
2018-03-26 09:08:34 +0300

    Create test.py

---
+++ test.py
@@ -0,0 +1,13 @@
+class Student():
+  def __init__(self,name,id):
+    self.name=name
+    self.id=id
+  def changeID(self,id):
+    self.id=id
+  def print1(self):
+      print("{}-{}".format(self.name,self.id))
+      
+kot=Student("kot",11)
+kot.print1()
+kot.changeID(12)
+kot.print1()
